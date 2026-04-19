c01a746b032fc90092a7ef393f583729b567f22e
Enji ExXor <87878908+coppfe@users.noreply.github.com>
2026-04-19 16:13:57 +0300

    Removed path

--- example/scan_lib.py
+++ example/scan_lib.py
@@ -3,7 +3,7 @@ from androcorn_native_tools.library.scanner import ELFScanner
 from androidemu.internal.elf_reader import ELFReader
 from androidemu.const.emu_const import ARCH_ARM64, ARCH_ARM32
 
-reader = ELFReader(r"C:\Users\Kirill\Desktop\androidemu\vfs\system\lib64\libc.so", demangle=False)
+reader = ELFReader(r"path", demangle=False)
 scanner = ELFScanner(reader, ARCH_ARM64)
 
 results_x0 = scanner.search_instructions("mrs", ["x0", None])
